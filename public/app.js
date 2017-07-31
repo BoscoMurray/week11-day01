2ca77f591718f38e75bea78c7c673223d0a8b857
BoscoMurray <rossamurray@gmail.com>
2017-07-31 21:44:17 +0100

    moved all html for cats into JS

--- public/app.js
+++ public/app.js
@@ -50,29 +50,13 @@ var addCat = function(name, faveFood, imgUrl) {
 
 var app = function () {
 
+  addCat("Boba", "Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
+
+  addCat("Barnaby", "Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+
+  addCat("Max", "Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
+
   addCat("reeceCat", "monsterMunch", "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg");
-
-  // var name = document.createElement("li");
-  // name.innerText = "Name: Boba";
-
-  // var faveFood = document.createElement("li");
-  // faveFood.innerText = "Favourite Food: Sock fluff";
-
-  // var img = document.createElement("img");
-  // img.width = 500;
-  // img.src = "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg";
-
-  // var imgLi = document.createElement("li");
-  // imgLi.appendChild(img);
-
-  // var cat = document.createElement("ul");
-  // cat.classList.add("cat");
-  // cat.appendChild(name);
-  // cat.appendChild(faveFood);
-  // cat.appendChild(imgLi);
-
-  // var cats = document.querySelector("#cats");
-  // cats.appendChild(cat);
 
 }
 
